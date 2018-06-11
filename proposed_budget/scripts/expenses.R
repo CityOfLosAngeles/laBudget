42944b90573b7f9c0a6d6bc8330e89404da961e5
aescherling <adam.scherling@gmail.com>
2018-06-11 16:38:10 -0700

    slight cleanup

--- proposed_budget/scripts/expenses.R
+++ proposed_budget/scripts/expenses.R
@@ -1,38 +1,63 @@
 # expenses.R
 # prep the expenses data and push to Socrata
 # Adam Scherling
-# April 18, 2018
+# Written April 18, 2018. Updated June 11, 2018.
 
-# load libraries
-library(dplyr)
-library(magrittr)
-library(readxl)
-library(RSocrata)
+## Setup
 
-# set working directory
-setwd('~/github/laBudget/proposed_budget/data/FY18-19')
+# load required packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr, magrittr, tidyr, lubridate, data.table, RSocrata, readxl)
 
-# read in the new expenses data
-sec2 <- read_excel("Expenditures_Sec2 All Regular Depts and NonDepts_1819Proposed.xlsx") %>% data.frame
-# sec4 <- read_excel("Expenditures_Sec4 Library and Rec Parks_1819Proposed.xlsx") %>% data.frame
+# set directories
+# data = directory where the data are saved
+# login = directory where Socrata login information is saved
+#         (username.txt, password.txt - these should be plain text files 
+#		   containing nothing but the username and password, respectively)
+dir <- list(data="~/github/laBudget/proposed_budget/data/FY18-19/",
+       login="~/github/laBudget/proposed_budget/scripts/")
 
-# merge into a single dataset
-# new_expenses <- merge(sec2, sec4, all=T)
-# rm(sec2, sec4)
+# set the working directory
+setwd(dir$data)
 
-new_expenses <- sec2
+# url for the dataset
+socrata_url <- "https://data.lacity.org/A-Prosperous-City/Open-Budget-Appropriations-Fiscal-Years-2010-2019/5242-pnmt"
+
+# API endpoint for the dataset
+socrata_endpoint <- "https://data.lacity.org/resource/ws4f-n5ax.json"
+
+# filename of Excel spreadsheet
+excel_file <- "Expenditures_Sec2 All Regular Depts and NonDepts_1819Proposed.xlsx"
+
+# get Socrata username and password for upload
+username <- readLines(paste0(dir$login, 'username.txt'))
+password <- readLines(paste0(dir$login, 'password.txt'))
+
+# get a timestamp
+timestamp <- gsub(':','.',now())
+timestamp <- gsub(' ','_',timestamp)
+
+# prevent R from writing to scientific notation
+options(scipen=999)
+
+
+## Read In Data
+
+
+# read in the new expenses data. section 2 only
+new_expenses <- read_excel(excel_file) %>% data.frame
 
 # remove rows with no 2018-2019 data
 new_expenses %<>% filter(!is.na(X2018.19.Proposed))
 
 # read in the existing data on Socrata
-old_expenses <- read.socrata("https://data.lacity.org/resource/ws4f-n5ax.json")
+old_expenses <- read.socrata(socrata_url)
 
 # filter out any 2019 data
 old_expenses %<>% filter(fiscal_year!=2019)
 
 # save a copy as a backup
-# write.csv(old_expenses, 'old_expenses.csv', row.names=F)
+write.csv(old_expenses, paste0('old_expenses_',timestamp,'.csv'), row.names=F)
 
 colnames(new_expenses)
 colnames(old_expenses)
@@ -43,25 +68,37 @@ new_expenses <- new_expenses[,-c(7,8,13,14,15)]
 
 # change the column names of the new data to match the old data
 # missing columns: Program_Priority, Expense_Type
-colnames(new_expenses) <- c("dept_code","department_name","subdept_code",
-	"subdepartment_name","prog_code","program_name","source_fund_code",
-	"source_fund_name","account_code","account_name","appropriation")
+colnames(new_expenses) <- c("Dept_Code","Department_Name","SubDept_Code",
+	"SubDepartment_Name","Prog_Code","Program_Name","Source_Fund_Code",
+	"Source_Fund_Name","Account_Code","Account_Name","Appropriation")
+
+
+## Get Program Priorities
+# assign a category to each expenditure, e.g. A Well Run City, A Safe City,...
+# an attempt is made to do this by matching the categories in the old dataset
+# to new observations with the same department and program codes.
+# unfortunately this doesn't work all that well
 
 # for existing program names, get the program priority
-df <- old_expenses %>% select(dept_code, prog_code, program_priority)
-df <- df[!duplicated(df),]
-df <- na.omit(df)
+priority <- old_expenses %>% select(Dept_Code, Prog_Code, Program_Priority)
+priority <- priority[!duplicated(priority),]
+priority <- na.omit(priority)
 
-# remove duplicated dept_code, prog_code combinations. 
+# remove duplicated Dept_Code, Prog_Code combinations. 
 # somewhat undiscriminating, can be refined later
-dd <- df %>% select(dept_code, prog_code) %>% duplicated
-df <- df[!dd,]
+dd <- priority %>% select(Dept_Code, Prog_Code) %>% duplicated
+priority <- priority[!dd,]
 
 # merge in the program priority and expense type
-new_expenses <- merge(new_expenses, df, by=c("dept_code","prog_code"), all.x=T)
+new_expenses <- merge(new_expenses, priority, by=c("Dept_Code","Prog_Code"), all.x=T)
 
 # examine the merge
 sapply(colnames(new_expenses), function(j) sum(is.na(new_expenses[,j])))
+
+
+
+### Miscellaneous data transformations
+
 
 # add a fiscal year column
 new_expenses$fiscal_year <- 2019
@@ -71,9 +108,6 @@ new_expenses$expense_type <- NA
 
 # arrange columns in the same order as the old expenses data
 new_expenses %<>% select(colnames(old_expenses))
-
-# prevent use of scientific notation when writing to csv
-options(scipen=999)
 
 # convert appropriation column to character
 new_expenses %<>% mutate(appropriation=as.character(appropriation))
@@ -86,13 +120,11 @@ expenses %<>% arrange(desc(fiscal_year), department_name, program_name, account_
 
 
 # write to csv
-write.csv(expenses, 'expenses.csv', row.names=F)
+# write.csv(expenses, 'expenses.csv', row.names=F)
 
-# # upload the data to Socrata
-# user_password <- readLines("~/github/laBudget/proposed_budget/scripts/password.txt")
-
-# write.socrata(dataframe = new_expenses,
-#               dataset_json_endpoint = "https://data.lacity.org/resource/ws4f-n5ax.json",
-#               update_mode = "REPLACE",
-#               email = "adam.scherling@lacity.org",
-#               password = user_password)
+# upload the data to Socrata
+write.socrata(dataframe = new_expenses,
+              dataset_json_endpoint = socrata_endpoint,
+              update_mode = "REPLACE",
+              email = username,
+              password = password)
--- proposed_budget/scripts/revenues.R
+++ proposed_budget/scripts/revenues.R
@@ -1,28 +1,64 @@
 # revenues.R
-# update the revenue data on Socrata. This presumes that the new data are already available as a csv
+# Update the revenue data on Socrata. 
+# It is presumed that the new data have already been converted from a pdf
+# to a csv. Actually, two csv files: one for GF and SF revenues, another for
+# available balances. The available balances will be added to revenues.
 # Adam Scherling
-# April 18, 2018
+# April 18, 2018. Updated June 11, 2018
 
-library(RSocrata)
-library(dplyr)
-library(magrittr)
+## Setup
+
+# load required packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr, magrittr, tidyr, lubridate, data.table, RSocrata, readxl)
+
+# set directories
+# data = directory where the data are saved
+# login = directory where Socrata login information is saved
+#         (username.txt, password.txt)
+dir <- list(data="~/github/laBudget/proposed_budget/data/FY18-19/",
+       login="~/github/laBudget/proposed_budget/scripts/")
 
 # set the working directory
-setwd('~/github/laBudget/proposed_budget/data/FY18-19')
+setwd(dir$data)
 
-# read in the old data and save as a backup
-old_revenues <- read.socrata("https://data.lacity.org/A-Prosperous-City/Open-Budget-Revenue-2010-2018/ih6g-qkwz/data")
-# write.csv(old_revenues, "old_revenues.csv", row.names=F)
-# old_revenues %<>% filter(Fiscal.Year.Shorthand!=2019)
+# url for the dataset
+socrata_url <- "https://data.lacity.org/A-Prosperous-City/Open-Budget-Revenue-2010-2018/ih6g-qkwz"
+
+# API endpoint for the dataset
+socrata_endpoint <- "https://data.lacity.org/resource/y3a7-9ent.json"
+
+# get Socrata username and password for upload
+username <- readLines(paste0(dir$login, 'username.txt'))
+password <- readLines(paste0(dir$login, 'password.txt'))
+
+# get a timestamp
+timestamp <- gsub(':','.',now())
+timestamp <- gsub(' ','_',timestamp)
+
+# prevent R from writing to scientific notation
+options(scipen=999)
+
+
+## Data
+
+# read in the data on Socrata and save as a backup
+old_revenues <- read.socrata(socrata_url)
+write.csv(old_revenues, paste0("old_revenues_",timestamp,".csv"), row.names=F)
+
+# filter out 2019 data
+old_revenues %<>% filter(Fiscal.Year.Shorthand!=2019)
 
 # read in the new data
 new_revenues <- read.csv('new_revenues.csv', stringsAsFactors=F)
 available_balances <- read.csv('available_balances.csv', stringsAsFactors=F)
 
 # add the available balances to the revenues
-new_revenues <- merge(new_revenues, available_balances, by="Revenue.Source", all=T)
+new_revenues <- merge(new_revenues, available_balances, 
+	by="Revenue.Source", all=T)
 new_revenues$Available.Balance[is.na(new_revenues$Available.Balance)] <- 0
-new_revenues %<>% mutate(Amount=as.numeric(Amount) + as.numeric(Available.Balance))
+new_revenues %<>% mutate(Amount=as.numeric(Amount) + 
+	as.numeric(Available.Balance))
 
 # remove the available balances column
 new_revenues %<>% select(-Available.Balance)
@@ -33,8 +69,13 @@ revenues <- rbind(new_revenues, old_revenues)
 # order by fiscal year, fund type, name
 revenues %<>% arrange(desc(Fiscal.Year.Shorthand), Fund.Type, Revenue.Source)
 
-# prevent R from writing to scientific notation
-options(scipen=999)
 
 # write to csv
-write.csv(revenues, 'revenues.csv', row.names=F)
+# write.csv(revenues, 'revenues.csv', row.names=F)
+
+# write to socrata
+write.socrata(dataframe = revenues,
+              dataset_json_endpoint = socrata_endpoint,
+              update_mode = "REPLACE",
+              email = username,
+              password = password)
